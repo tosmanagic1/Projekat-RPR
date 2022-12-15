75d9b7d2b870716c686d8bd2a8c56535b0ddce8a
tosmanagic1 <tosmanagic1@etf.unsa.ba>
2022-12-16 00:52:49 +0100

    Initial commit

---
+++ src/test/java/ba/unsa/etf/rpr/MainTest.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
